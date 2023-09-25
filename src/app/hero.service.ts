b6a7b593cafdbff2b18b8879e3ff33632dc0fde5
Jarett Forzano <jpforzanojr@gmail.com>
2023-09-24 21:11:13 -0400

    first commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageService {
+  messages: string[] = [];
+
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
+}
